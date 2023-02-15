128dc741716013c11b47c15b29bf1edbe3bf1968
hwalker928 <harry@harryw.link>
2023-02-15 20:14:00 +0000

    Add code styling workflow

--- python/web-servers/flask-example/main.py
+++ python/web-servers/flask-example/main.py
@@ -13,4 +13,4 @@ def hello():
 
 if __name__ == "__main__":
     load_dotenv()
-    app.run(host="0.0.0.0", port=os.getenv('SERVER_PORT'))
+    app.run(host="0.0.0.0", port=os.getenv("SERVER_PORT"))
