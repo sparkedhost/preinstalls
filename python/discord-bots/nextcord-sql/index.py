c51606b98921f1a822ba9bc872d615c0154a1ae5
Harry W <harry@harryw.link>
2023-02-15 19:35:11 +0000

    Reformat code with Black

--- python/discord-bots/nextcord-sql/index.py
+++ python/discord-bots/nextcord-sql/index.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 
 
-TESTING_GUILD_ID = 1234567890 # change to example 
+TESTING_GUILD_ID = 1234567890  # change to example
 
 description = """An example bot sparkedhost preinstalls
 
@@ -16,6 +16,7 @@ Author:
 """
 
 bot = commands.Bot(description=description)
+
 
 @bot.event
 async def on_ready():
@@ -28,31 +29,40 @@ async def on_ready():
         database="nextcord",
     )
     cur = con.cursor()
-    await cur.execute("""
+    await cur.execute(
+        """
     CREATE TABLE IF NOT EXISTS example_table(
         id SERIAL PRIMARY KEY,
         value TEXT
     )
-    """)
-    print(f'Logged in as {bot.user}')
+    """
+    )
+    print(f"Logged in as {bot.user}")
+
 
 @bot.slash_command(description="get the count", guild_ids=[TESTING_GUILD_ID])
 async def get_data(interaction: nextcord.Interaction):
     cur = con.cursor()
-    await cur.execute("""
+    await cur.execute(
+        """
     SELECT count(*) FROM example_table
-    """)
+    """
+    )
     ret = await cur.fetchone()
     await interaction.send(f"The data stored in the database is: {ret[0]}")
+
 
 @bot.slash_command(description="increment the count by 1", guild_ids=[TESTING_GUILD_ID])
 async def inc_data(interaction: nextcord.Interaction):
     cur = con.cursor()
-    await cur.execute("""
+    await cur.execute(
+        """
     INSERT INTO example_table (value) VALUES (1)
-    """)
+    """
+    )
     await con.commit()
     await interaction.send("Set Value!")
+
 
 load_dotenv()
 bot.run(os.getenv("BOT_TOKEN"))
