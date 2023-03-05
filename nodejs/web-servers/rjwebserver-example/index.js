189a1566e27a1ac3d66510019f6c4891c1213c9d
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2023-03-05 15:21:55 +0000

    Update rjweb-server Example

--- nodejs/web-servers/rjwebserver-example/index.js
+++ nodejs/web-servers/rjwebserver-example/index.js
@@ -1,18 +1,22 @@
 // Define all the modules that we will be using in this file.
-const webserver = require('rjweb-server')
+const { Server } = require('rjweb-server')
 require('dotenv').config();
 
 // Define Routes for the web server
-const routes = new webserver.routeList()
+const server = new Server({
+  port: process.env.SERVER_PORT,
+  compression: 'gzip',
+  proxy: true
+})
 
-routes.routeBlock('/') //*1
+server.prefix('/') //*1
   .add('GET', '/', (ctr) => {
     ctr.print('Hello world!')
   })
   .loadCJS('./routes') // This will load all Routes from the Routes folder and add the prefix / (*1) to them
 
-routes.routeBlock('/api')
-  .auth((ctr) => { // This will authenticate routes that are defined below
+server.prefix('/api')
+  .validate((ctr) => { // This will validate routes that are defined below
     if (!ctr.queries.has('auth')) return ctr.status(422).print('Auth Query (?auth=) is missing!')
     if (ctr.queries.get('auth') !== '123') return ctr.status(401).print('Auth Query (?auth=) is not 123')
 
@@ -21,8 +25,8 @@ routes.routeBlock('/api')
   .add('GET', '/', (ctr) => { // This will be available on /api since it combines every prefix to the top
     ctr.print('Welcome to the API!')
   })
-  .subRouteBlock('/user')
-    .auth((ctr) => { // Requires Additional Authentication, not just the one above
+  .prefix('/user')
+    .validate((ctr) => { // Requires Additional Validation, not just the one above
       if (!ctr.queries.has('user')) return ctr.status(422).print('User Query (?user=) is missing!')
 
       return ctr.status(200) // If everything is valid, return 200 status (ok)
@@ -34,28 +38,21 @@ routes.routeBlock('/api')
       })
     })
 
-routes.routeBlock('/homepage')
+server.prefix('/homepage')
   .static('./static', {
     hideHTML: true, // This will automatically hide the .html extension from pages, so /index.html -> /, /blog.html -> /blog
     addTypes: true // This will automatically add Content-Types for Javascript, etc
   })
 
-routes.event('notfound', (ctr) => {
+server.event('notfound', (ctr) => {
   ctr.status(404)
   ctr.print('This page was not found!')
 })
 
-routes.event('request', (ctr) => {
+server.event('request', (ctr) => {
   console.log(`${ctr.url.method} Request made to ${ctr.url.href} by ${ctr.client.ip}`)
 })
 
-const controller = webserver.initialize({
-  port: process.env.SERVER_PORT,
-  compression: 'gzip',
-  proxy: true
-})
-
-controller.setRoutes(routes)
-controller.start().then((res) => {
+server.start().then((res) => {
   console.log(`server listening on port ${res.port}`)
 })
--- nodejs/web-servers/rjwebserver-example/routes/say.js
+++ nodejs/web-servers/rjwebserver-example/routes/say.js
@@ -1,7 +1,7 @@
 /** @type {import('rjweb-server/interfaces').CtrFile} */
 module.exports = {
 	method: 'GET',
-	path: '/say/:word:', // This Route will repeat what the user specified as parameter word
+	path: '/say/<word>', // This Route will repeat what the user specified as parameter word
 
 	async code(ctr) {
 		ctr.print(ctr.params.get('word'))
